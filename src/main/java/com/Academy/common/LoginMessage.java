e54fdee70e0054c0cee1f9270d43403ba636af17
Kishore K <kishorekarthik2003@gmail.com>
2024-11-20 19:00:26 +0530

    login commit

--- src/main/java/com/Academy/common/LoginMessage.java
+++ src/main/java/com/Academy/common/LoginMessage.java
@@ -9,6 +9,11 @@ public class LoginMessage {
     private String message;
     private Boolean status;
     private Boolean success;
+    // Constructor
+    public LoginMessage(String message, boolean success) {
+        this.message = message;
+        this.success = success;
+    }
     public boolean isSuccess() {
         return success;
     }
